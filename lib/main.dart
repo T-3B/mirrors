ce33046a94dd37af4b0ebfb9211bf1ccc69d7ebf
T-3B <cle.smagghe@gmail.com>
2023-11-18 01:52:57 +0100

    Level "controller", with collisions

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mirrors/controllers/level.dart';
 import 'package:mirrors/models/home/home_assets.dart';
-import 'package:mirrors/views/gamePlay/game_view.dart';
-import 'package:mirrors/views/level/level.dart';
 import 'package:mirrors/views/menu/about.dart';
 import 'package:mirrors/views/menu/home.dart';
 import 'package:mirrors/views/menu/settings.dart';
@@ -27,7 +26,7 @@ class MainApp extends StatelessWidget {
         'home': (context) => const Home(),
         'about': (context) => const About(),
         'settings': (context) => const Settings(),
-        'play': (context) => const GameView()
+        'play': (context) => const LevelController(levelID: 1)  // TODO provide the levelID from the selected level by the user
       },
       home: Scaffold(
         body: ChangeNotifierProvider(
